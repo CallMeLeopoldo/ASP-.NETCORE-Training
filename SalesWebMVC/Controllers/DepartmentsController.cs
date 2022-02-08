2f9b53f03bbfa5b42e930f47748179f48ab13729
Leandro Salgado <leandro.salgado@reditpro.com>
2022-02-08 15:45:32 +0000

    Added Model, Controller and View for Department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+            list.Add(new Department { Id = 3, Name = "Gastronomics" });
+
+            Console.WriteLine(list.Count == 0);
+            
+            return View(list);
+        }
+    }
+}
